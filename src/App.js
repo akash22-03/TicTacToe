fcd43576a65f9830de9a8fef39f48696bec9fda0
akash22-03 <akashdash2203@gmail.com>
2021-09-22 00:19:59 +0530

    Logic build successfull

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ import StatusMessage from './components/StatusMessage';
 import { calculateWinner } from "./helpers";
 
 import "./styles/root.scss"
+
+const NEW_GAME = [{ board: Array(9).fill(null), isXNext: true },];
+
 const App = () => {
   const [history, setHistory] = useState([{ board: Array(9).fill(null), isXNext: true },]);
 
   const [currentMove, setCurrentMove] = useState(0);
   const current = history[currentMove];
-  const winner = calculateWinner(current.board);
+  const { winner, winningSquares } = calculateWinner(current.board);
 
 
   const handleSquareClick = position => {
@@ -36,11 +39,17 @@ const App = () => {
     setCurrentMove(move);
   }
 
+  const onNewGame = () => {
+    setHistory(NEW_GAME);
+    setCurrentMove(0);
+  };
+
   return (
     <div className="app">
       <h1>TIC TAC TOE </h1>
       <StatusMessage winner={winner} current={current} />
-      <Board board={current.board} handleSquareClick={handleSquareClick} />
+      <Board board={current.board} handleSquareClick={handleSquareClick} winningSquares={winningSquares} />
+      <button type="button" onClick={onNewGame}>Start new game</button>
       <History history={history} moveTo={moveTo} currentMove={currentMove} />
     </div>
   );
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
 
-const square = ({ value, onClick }) => {
+const square = ({ value, onClick, isWinningSquare }) => {
   return (
-    <button type="button" className="square" onClick={onClick}>
+    <button type="button" className="square" onClick={onClick} style={{ fontWeight: isWinningSquare ? 'bold' : 'normal' }}>
       {value}
     </button>
   );
