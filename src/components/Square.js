d03bb0f3c679a59d15435402ab785d546f7b109b
akash22-03 <akashdash2203@gmail.com>
2021-09-21 11:23:21 +0530

    added sass

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,7 +1,8 @@
 import React from 'react'
 
+
 const square = ({ value }) => {
-  return <button type="button">{value}</button>;
+  return <button type="button" className='square'>{value}</button>;
 }
 
 export default square
