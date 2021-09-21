0a7eaaae4cf5b8c54cf8e88a4e4873b2d5671db2
akash22-03 <akashdash2203@gmail.com>
2021-09-21 16:58:50 +0530

    display draw message

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Board from './components/Board';
 import History from './components/History';
+import StatusMessage from './components/StatusMessage';
 import { calculateWinner } from "./helpers";
 
 import "./styles/root.scss"
@@ -10,7 +11,7 @@ const App = () => {
   const [currentMove, setCurrentMove] = useState(0);
   const current = history[currentMove];
   const winner = calculateWinner(current.board);
-  const message = winner ? `Winner is ${winner}` : `Next Player is ${current.isXNext ? 'X' : 'O'}`;
+
 
   const handleSquareClick = position => {
     if (current.board[position] || winner) {
@@ -38,13 +39,10 @@ const App = () => {
   return (
     <div className="app">
       <h1>TIC TAC TOE </h1>
-      <h2>{message}</h2>
+      <StatusMessage winner={winner} current={current} />
       <Board board={current.board} handleSquareClick={handleSquareClick} />
       <History history={history} moveTo={moveTo} currentMove={currentMove} />
     </div>
   );
-
 };
-
-
 export default App;
